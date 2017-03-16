11446922219d63b04f9fd3e574bb6593012d9155
Lee youngtaek <ting4602@gmail.com>
2017-03-16 22:32:37 +0900

    project:first_commit

---
+++ CtrlFF/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CtrlFF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.btnFindPath = new System.Windows.Forms.Button();
+            this.tbBefore = new System.Windows.Forms.TextBox();
+            this.tbAfter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.ReadOnly = true;
+            this.tbFolderPath.Size = new System.Drawing.Size(287, 21);
+            this.tbFolderPath.TabIndex = 0;
+            // 
+            // btnFindPath
+            // 
+            this.btnFindPath.Location = new System.Drawing.Point(305, 10);
+            this.btnFindPath.Name = "btnFindPath";
+            this.btnFindPath.Size = new System.Drawing.Size(75, 23);
+            this.btnFindPath.TabIndex = 1;
+            this.btnFindPath.Text = "Find";
+            this.btnFindPath.UseVisualStyleBackColor = true;
+            this.btnFindPath.Click += new System.EventHandler(this.btnFindPath_Click);
+            // 
+            // tbBefore
+            // 
+            this.tbBefore.Location = new System.Drawing.Point(71, 49);
+            this.tbBefore.Name = "tbBefore";
+            this.tbBefore.Size = new System.Drawing.Size(228, 21);
+            this.tbBefore.TabIndex = 2;
+            // 
+            // tbAfter
+            // 
+            this.tbAfter.Location = new System.Drawing.Point(71, 78);
+            this.tbAfter.Name = "tbAfter";
+            this.tbAfter.Size = new System.Drawing.Size(228, 21);
+            this.tbAfter.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Before : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "After : ";
+            // 
+            // btnFix
+            // 
+            this.btnFix.Location = new System.Drawing.Point(305, 76);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(75, 23);
+            this.btnFix.TabIndex = 6;
+            this.btnFix.Text = "Fix";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 109);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAfter);
+            this.Controls.Add(this.tbBefore);
+            this.Controls.Add(this.btnFindPath);
+            this.Controls.Add(this.tbFolderPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Button btnFindPath;
+        private System.Windows.Forms.TextBox tbBefore;
+        private System.Windows.Forms.TextBox tbAfter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFix;
+    }
+}
+
---
+++ CtrlFF/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace CtrlFF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string path = dialog.SelectedPath;
+
+            tbFolderPath.Text = path;
+        }
+
+        private void btnFix_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(tbFolderPath.Text))
+            {
+                MessageBox.Show("Folder Path Error");
+                return;
+            }
+
+            if(string.IsNullOrEmpty(tbBefore.Text) || string.IsNullOrEmpty(tbAfter.Text))
+            {
+                MessageBox.Show("Text Error");
+                return;
+            }
+
+            ChangeTextAtFolder(tbFolderPath.Text, tbBefore.Text, tbAfter.Text);
+        }
+
+        private void ChangeTextAtFolder(string path, string beforeText, string afterText)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach(var file in di.GetFiles())
+            {
+                if (file.Name.Contains(beforeText))
+                {
+                    file.MoveTo(file.Directory.FullName + "\\" + file.Name.Replace(beforeText, afterText));
+                }
+            }
+
+            foreach(var directory in di.GetDirectories())
+            {
+                ChangeTextAtFolder(directory.FullName, beforeText, afterText);
+            }
+
+            MessageBox.Show("SUCCESSFUL");
+
+            tbBefore.Text = "";
+            tbAfter.Text = "";
+            tbFolderPath.Text = "";
+
+            return;
+        }
+    }
+}
